bd163380eed4af478607ce7ef6ddfa34ab320b94
tdna <ta1986@gmail.com>
2016-04-05 18:43:47 +0200

    Replaced cmd to a more abstract argument called arg. Moved docker actions to mdocker module. Added top and logs action.

--- mexec/mdocker.py
+++ mexec/mdocker.py
@@ -4,7 +4,19 @@ class Docker(object):
     def __init__(self, cli):
         self.cli = cli
 
-    def containers_by_env_variable(self, env_variable, callback):
+    def logs(self, container, **kwargs):
+        return self.cli.logs(container, **kwargs)
+
+    def top(self, container, **kwargs):
+        return self.cli.top(container, **kwargs)
+
+    def inspect(self, container, **kwargs):
+        return self.cli.inspect_container(container, **kwargs)
+
+    def exec_container(self, container, cmd):
+        return self.cli.exec_start(self.cli.exec_create(container, cmd))
+
+    def exec_by_env_variable(self, env_variable, callback):
         def have_env(container):
             env_vars = self.cli.inspect_container(container)['Config']['Env']
             return env_variable in set(env_vars)
